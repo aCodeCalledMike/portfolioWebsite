cb5fbc4a90ae61246ea2a72cb13dacf89981d359
MikeH <hobbespostart@gmail.com>
2021-09-16 15:10:47 +0100

    added new CSS and JS

---
+++ portfolioJS.js
@@ -0,0 +1,9 @@
+ function showAbout() {
+        var x = document.getElementById("about-page");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      }
+;
